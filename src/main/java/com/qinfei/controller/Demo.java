1edc8402bcdff4a337d5ad4da851830467ef4706
qinfei <2232290419@qq.com>
2020-02-10 13:34:25 +0800

    hello页面

---
+++ src/main/java/com/qinfei/controller/Demo.java
@@ -0,0 +1,14 @@
+package com.qinfei.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Demo {
+
+	@RequestMapping("list")
+	public String list(){
+		
+		return "list";
+	}
+}
